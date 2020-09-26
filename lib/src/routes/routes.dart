db7438f1a99651ef417a6977a6ca7dc4a90e0b9e
gallottohugo <gallottohugo@gmail.com>
2020-09-26 13:48:30 -0300

    Add transaction page to route

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:test_inc/src/pages/client_detail_page.dart';
 import 'package:test_inc/src/pages/client_list_page.dart';
+import 'package:test_inc/src/pages/transaction_list_page.dart';
 
 class ApplicationRoutes {
 	static Map<String, WidgetBuilder> getApplicationRoutes(){
 		return <String, WidgetBuilder> {
-			ClientDetailPage.routeName         : (BuildContext context) => ClientDetailPage(),
-			ClientListPage.routeName        : (BuildContext context) => ClientListPage(),
+			ClientDetailPage.routeName     : (BuildContext context) => ClientDetailPage(),
+			ClientListPage.routeName       : (BuildContext context) => ClientListPage(),
+			TransactionListPage.routeName  : (BuildContext context) => TransactionListPage(),
 		};
 	}
 }
