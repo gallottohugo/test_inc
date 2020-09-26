2e1e516415ef15916e20833a51a9ac55c97bdf92
gallottohugo <gallottohugo@gmail.com>
2020-09-26 13:46:23 -0300

    Create transaction model

---
+++ lib/src/models/transaction_model.dart
@@ -0,0 +1,10 @@
+class TransactionModel{
+	int tipoDeRegistro;
+	String idTransaccion;
+	double monto;
+	String reservado;
+	int tipo;
+	String clientId;
+	static List<TransactionModel> listTransactions = new List<TransactionModel>();
+
+}
