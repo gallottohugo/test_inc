4fa7282fa3389517da41e7efc5763419e863b7f3
gallottohugo <gallottohugo@gmail.com>
2020-09-26 11:55:07 -0300

    Create client pages

---
+++ lib/src/pages/client_detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ClientDetailPage extends StatefulWidget {
+	static final String routeName = 'client_detail_page';
+  	@override
+  	_ClientDetailPageState createState() => _ClientDetailPageState();
+}
+
+class _ClientDetailPageState extends State<ClientDetailPage> {
+  	@override
+  	Widget build(BuildContext context) {
+    	return Scaffold();
+  	}
+}
---
+++ lib/src/pages/client_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClientListPage extends StatefulWidget {
+	static final String routeName = 'client_list_page';
+  	ClientListPage({Key key}) : super(key: key);
+
+  	_ClientListPageState createState() => _ClientListPageState();
+}
+
+class _ClientListPageState extends State<ClientListPage> {
+  	@override
+  	Widget build(BuildContext context) {
+		return Scaffold();
+  	}
+}
